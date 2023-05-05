e4e2cfa078dfdf6a1b9c527357d07706b7e1cb89
sweta pandey <74544867+swetapandey-12@users.noreply.github.com>
2023-05-05 18:56:27 +0530

    Create Spelling to Number.py

---
+++ Spelling to Number.py
@@ -0,0 +1,36 @@
+a=input()
+def Num(query):
+    d={"One":'1',
+        "Two":'2',
+        "Three":'3',
+        "Four":'4',
+        "Five":'5',
+        "Six":'6',
+        "Seven":'7',
+        "Eight":'8',
+        "Nine":'9',
+        "Ten":'10'
+      }
+    return d[query]
+if a=="One":
+    print(Num(a))
+elif a=="Two":
+    print(Num(a))
+elif a=="Three":
+    print(Num(a))
+elif a=="Four":
+    print(Num(a))
+elif a=="Five":
+    print(Num(a))
+elif a=="Six":
+    print(Num(a))
+elif a=="Seven":
+    print(Num(a))
+elif a=="Eight":
+    print(Num(a))
+elif a=="Nine":
+    print(Num(a))
+elif a=="Ten":
+    print(Num(a))
+else:
+    print("Out of syllabus")
